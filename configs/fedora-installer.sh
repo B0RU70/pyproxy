c558daf4251c09f593511ca38245f9a61efb540a
TheRabbit <39714901+B0RU70@users.noreply.github.com>
2019-04-17 03:11:18 +0300

    Add files via upload

---
+++ configs/fedora-installer.sh
@@ -0,0 +1,12 @@
+echo "Fedora Auto Installer Running.."
+IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+dnf update
+dnf install python
+dnf install nohup
+dnf install python-pip
+pip install --upgrade configs/proxy.py
+python configs/setup.py
+clear
+clear
+echo "Close Putty Your Proxy is=  $IP : 8080"
+nohup python configs/proxy.py --hostname $IP --port 8080 >> proxlog.txt &
